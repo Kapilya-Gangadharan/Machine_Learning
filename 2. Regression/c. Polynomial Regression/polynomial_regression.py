4702b0e3e6b4b497a445ffd80f79755702815aa3
kapilya <kapilya@gmail.com>
2023-02-19 11:30:14 -0500

    Regression Model

--- 2. Regression/c. Polynomial Regression/polynomial_regression.py
+++ 2. Regression/c. Polynomial Regression/polynomial_regression.py
@@ -30,13 +30,13 @@ fig, axes = plt.subplots(nrows=1, ncols=3)
 # Visualising the Linear Regression results
 axes[0].scatter(X, y, color = 'red')
 axes[0].plot(X, lin_reg.predict(X), color = 'blue')
-axes[0].set(title ='Truth or Bluff (Linear Regression)', xlabel = ('Position Level'), ylabel = 'Salary')
+axes[0].set(title ='Linear Regression', xlabel = ('Position Level'), ylabel = 'Salary')
 #plt.show()
 
 # Visualising the Polynomial Regression results
 axes[1].scatter(X, y, color = 'red')
 axes[1].plot(X, lin_reg_2.predict(poly_reg.fit_transform(X)), color = 'blue')
-axes[1].set(title= 'Truth or Bluff (Polynomial Regression)', xlabel = 'Position level', ylabel = 'Salary')
+axes[1].set(title= 'Polynomial Regression', xlabel = 'Position level', ylabel = 'Salary')
 #plt.show()
 
 # Visualising the Polynomial Regression results (for higher resolution and smoother curve)
@@ -44,7 +44,7 @@ X_grid = np.arange(min(X), max(X), 0.1)
 X_grid = X_grid.reshape((len(X_grid), 1))
 axes[2].scatter(X, y, color = 'red')
 axes[2].plot(X_grid, lin_reg_2.predict(poly_reg.fit_transform(X_grid)), color = 'blue')
-axes[2].set(title = 'Truth or Bluff (Polynomial Regression)', xlabel=  'Position level', ylabel = 'Salary')
+axes[2].set(title = 'Polynomial Regression - Higher Resolution', xlabel=  'Position level', ylabel = 'Salary')
 plt.show()
 
 # Predicting a new result with Linear Regression
